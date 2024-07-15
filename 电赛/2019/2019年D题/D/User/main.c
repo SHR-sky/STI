9c9fce260e8d77e903c76284016cabcc33a5f8cc
SHR-sky <1779558876@qq.com>
2024-07-15 17:39:47 +0800

    2019D

--- 电赛/2019/2019年D题/D/User/main.c
+++ 电赛/2019/2019年D题/D/User/main.c
@@ -1,8 +1,8 @@
 #include "sys.h"
 
-
-const double R_stand = 100.2;
-const double VDC_in = 3.103;
+const double R_stand = 16.8;
+const double RL = 1.6;
+double VDC_in;
 
 double V_stand;
 double Vin;
@@ -21,12 +21,6 @@ int Fre_down;
 
 int main()
 {
-	
-	float EcgDiscrList[28];
-	float stdResultDiffECG;
-
-	arm_std_f32(EcgDiscrList,28-4,&stdResultDiffECG);
-	
 	Adc_Init();
 
 	AD9959_Init();
@@ -36,45 +30,73 @@ int main()
 	
 	IO_Update(); 
 
-
 }
 
 
 
-int a[111];
-int b[222];
+double get_Vpp(ADC_TypeDef* kADCx,u8 ch)
+{
+	double Vmax;
+	double Vmin;
+	double V;
 
+	for(int i=0; i<1000; i++)
+	{
+		V = Get_Adc(kADCx,ch);
+		if(V>Vmax)
+			Vmax = V;
+		else if(V<Vmin)
+			Vmin = V;
+	}
+	return Vmax - Vmin;
+}
+
+void Open_cir(void)
+{
+	// 继电器开关打开
+}
+
+void Close_cir(void)
+{
+	// 继电器开关闭合
+}
 
 void Cal_Rin(void)
 {
 	// 非常低效的峰峰值检测
 
-	Vin = Get_Adc_Average(ADC1, CH1, 100) * 3.3  / 4096.0 ;
-	V_stand = Get_Adc_Average(ADC1, CH2, 100) * 3.3 / 4096.0 ;
-
-	Iin = ( (V_stand- VDC_in) - (Vin- VDC_in) ) / R_stand;
-
-	Rin = 0.5 / Iin;
+	V_stand = get_Vpp(ADC1, CH1);
+	Vin = get_Vpp(ADC2, CH1);
+	
+	Rin = Vin * R_stand / (V_stand-Vin);
 }
 
 void Cal_Rout(void)
 {
-
+	Open_cir();
+	double Voff = Get_Adc(ADC1,CH2);
+	Close_cir();
+	double Von = Get_Adc(ADC1,CH2);
+	Rout = (Voff - Von) * RL / Von;
 }
 
 void Cal_Av(void)
 {
+	// AD FFT
+	// 快速傅里叶之后，再去求得第一谐波的幅度
 
+
+
+	// 检波器
+	// 检出为真有效值，再去运算
 }
 
-void Cal_Curve(void)
+void Cal_Fre_up(void)
 {
 	for(int i=1000;; i++)
 	{
 		AD9959_Set_Fre(CH0,i);
 		IO_Update();
-
-
 
 		if(Av < 0.7071)
 		{
@@ -84,6 +106,23 @@ void Cal_Curve(void)
 	}
 }
 
+void Cal_curve(void)
+{
+	for(int i=1;i<10000000; i++)
+	{
+		AD9959_Set_Fre(CH0,i);
+		IO_Update();
+		Cal_Av();
+		Serial_Printf("va1.val=%d",Av);
+		Serial_Printf("va2.val=%d",100*(Av-(int)Av));
+	}
+}
+
+
+void judge_cir(void)
+{
+
+}
 
 
 
